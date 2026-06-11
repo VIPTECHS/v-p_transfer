9a782f92064bc8a45acfae6ca176f7c020ba4074
Emre Gundogdu <emre.gundogdu016@gmail.com>
2026-06-11 03:16:18 +0300

    Change search bar delay to 5 seconds

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSearch(true);
-    }, 4000);
+    }, 5000);
     return () => clearTimeout(timer);
   }, []);
 
